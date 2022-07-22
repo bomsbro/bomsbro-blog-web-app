a9f03b8455e77774e710245f60c659b0f8f91d5c
Jaehun Park <50619560+bomsbro@users.noreply.github.com>
2022-07-22 12:44:29 +0900

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains="bomsbro.com www.bomsbro.com"
 rsa_key_size=4096
-data_path="/boot/data/certbot"
+data_path="/var/data/certbot"
 email="transcend716@naver.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
