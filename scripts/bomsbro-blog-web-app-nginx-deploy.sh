6cdfc2fd0ac435b43e7e1e5e9a18c6077bb187bb
Jaehun Park <50619560+bomsbro@users.noreply.github.com>
2022-08-30 12:32:16 +0900

    Update bomsbro-blog-web-app-nginx-deploy.sh

--- scripts/bomsbro-blog-web-app-nginx-deploy.sh
+++ scripts/bomsbro-blog-web-app-nginx-deploy.sh
@@ -17,7 +17,8 @@ else
 fi
 
 sshpass -p $REMOTE_PASSWORD ssh -p $REMOTE_SSH_PORT $REMOTE_USERNAME@$REMOTE_HOST -o StrictHostKeyChecking=no << 'EOF'
-  docker rm -f ${IMAGE_REPOSITORY} || true;
+  dpcler stop -f ${IMAGE_REPOSITORY};
+  docker rm -f ${IMAGE_REPOSITORY};
   docker run -d --net=host --name ${IMAGE_REPOSITORY} --restart=always ${IMAGE_REPOSITORY};
   exit;
 EOF
