5675404e996cfe9bc6b4d133f148086dc3d55454
jaehun_park2 <jaehun_park2@tmax.co.kr>
2022-08-30 14:20:29 +0900

    :wrench: 배포스크립트 수정

--- scripts/bomsbro-blog-web-app-nginx-deploy.sh
+++ scripts/bomsbro-blog-web-app-nginx-deploy.sh
@@ -17,9 +17,7 @@ else
 fi
 
 sshpass -p $REMOTE_PASSWORD ssh -p $REMOTE_SSH_PORT $REMOTE_USERNAME@$REMOTE_HOST -o StrictHostKeyChecking=no << EOF
-	echo $IMAGE_REPOSITORY;
-	docker stop $IMAGE_REPOSITORY || true;
-	docker rm -f $IMAGE_REPOSITORY || true;
-	docker run -d --net=host --name $IMAGE_REPOSITORY --restart always $IMAGE_REPOSITORY;
-	exit;
+	docker rm -f $IMAGE_REPOSITORY || true
+	docker run -d --net host --name $IMAGE_REPOSITORY --restart always $IMAGE_REPOSITORY
+	exit
 EOF
