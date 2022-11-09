1b29fd43995f731be1c9a39b9d5278c39df6f4c7
Jaehun Park <50619560+bomsbro@users.noreply.github.com>
2022-11-09 17:54:09 +0900

    Update bomsbro-blog-web-app-nginx-deploy.sh

--- scripts/bomsbro-blog-web-app-nginx-deploy.sh
+++ scripts/bomsbro-blog-web-app-nginx-deploy.sh
@@ -8,16 +8,18 @@ REMOTE_USERNAME="$3"
 REMOTE_PASSWORD="$4"
 IMAGE_REPOSITORY="$5"
 
-if [[ $(sshpass -p $REMOTE_PASSWORD ssh -p $REMOTE_SSH_PORT $REMOTE_USERNAME@$REMOTE_HOST -o StrictHostKeyChecking=no "docker images | grep $IMAGE_REPOSITORY | tr -s ' ' | cut -d ' ' -f 3") != $(docker images $IMAGE_REPOSITORY | grep $IMAGE_REPOSITORY | tr -s ' ' | cut -d ' ' -f 3) ]]
-then
-	echo "$IMAGE_REPOSITORY image changed, updating..."
-	docker save $IMAGE_REPOSITORY | bzip2 | sshpass -p $REMOTE_PASSWORD ssh -p $REMOTE_SSH_PORT $REMOTE_USERNAME@$REMOTE_HOST -o StrictHostKeyChecking=no 'bunzip2 | docker load'
-else
-	echo "$IMAGE_REPOSITORY image did not change"
-fi
+echo "$IMAGE_REPOSITORY image changed, updating..."
+docker save $IMAGE_REPOSITORY | bzip2 | sshpass -p $REMOTE_PASSWORD ssh -p $REMOTE_SSH_PORT $REMOTE_USERNAME@$REMOTE_HOST -o StrictHostKeyChecking=no 'bunzip2 | docker load'
 
 sshpass -p $REMOTE_PASSWORD ssh -p $REMOTE_SSH_PORT $REMOTE_USERNAME@$REMOTE_HOST -o StrictHostKeyChecking=no << EOF
 	docker rm -f $IMAGE_REPOSITORY || true
 	docker run -d --net host --name $IMAGE_REPOSITORY --restart always -v /var/data/certbot/conf:/etc/letsencrypt -v /var/data/certbot/www:/var/www/certbot $IMAGE_REPOSITORY
+	expect -c "
+		set timeout 5
+		spawn docker image prune
+		expect -nocase \"\[y/N\]\"
+		send \"y\"
+		exit 0
+	"
 	exit
 EOF
